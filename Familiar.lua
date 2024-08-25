b019698e13359f246db0887b1dd3c68a1805d3d5
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2024-08-25 08:17:16 -0800

    Add files via upload

--- Familiar.lua
+++ Familiar.lua
@@ -563,6 +563,101 @@ SMODS.Joker {
     end
 }
 SMODS.Joker {
+    key = 'neopolitan',
+    config = {
+        extra = {chips = 50, chip_mod = 10 , mult = 10, mult_mod = 2, money = 5, money_mod = 1},
+    },
+    atlas = 'Joker',
+    pos = { x = 14, y = 10 },
+    loc_txt = {
+        ['en-us'] = {
+            name = 'Neopolitan',
+            text = {
+                "{C:blue}+#3#{} Chips or {C:mult}+#1#{} Mult or {C:money}+$#5#{}",
+                "{C:blue}-#4#{} Chips, {C:mult}-#2#{} Mult, and {C:money}-$#6#{}",
+                "for every hand played",
+            }
+        }
+    },
+    rarity = 2,
+    cost = 6,
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.mult, card.ability.extra.mult_mod, card.ability.extra.chips, card.ability.extra.chip_mod, card.ability.extra.money, card.ability.extra.money_mod} }
+    end,
+    calculate = function(self, card, context)
+        if context.after and not context.blueprint then
+            if card.ability.extra.mult - card.ability.extra.mult_mod <= 0 or card.ability.extra.chips - card.ability.extra.chip_mod <= 0 or card.ability.extra.money - card.ability.extra.money_mod <= 0 then 
+                G.E_MANAGER:add_event(Event({
+                    func = function()
+                        play_sound('tarot1')
+                        card.T.r = -0.2
+                        card:juice_up(0.3, 0.4)
+                        card.states.drag.is = true
+                        card.children.center.pinch.x = true
+                        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+                            func = function()
+                                    G.jokers:remove_card(card)
+                                    card:remove()
+                                    card = nil
+                                return true; end})) 
+                        return true
+                    end
+                })) 
+                return {
+                    message = localize('k_melted_ex'),
+                    colour = G.C.RED
+                }
+            else
+                card.ability.extra.mult = card.ability.extra.mult - card.ability.extra.mult_mod
+                card.ability.extra.chips = card.ability.extra.chips - card.ability.extra.chip_mod
+                card.ability.extra.money = card.ability.extra.money - card.ability.extra.money_mod
+                if 1 == 1 then
+                    return {
+                        message = localize{type='variable',key='a_mult_minus',vars={card.ability.extra.mult_mod}},
+                        colour = G.C.RED,
+                    }
+                end
+                if 1 == 1 then
+                    return {
+                        message = localize{type='variable',key='a_chips_minus',vars={card.ability.extra.chip_mod}},
+                        colour = G.C.CHIPS,
+                    }
+                end
+                return {
+                    message = localize('-$')..card.ability.extra.money_mod,
+                    colour = G.C.MONEY
+                }
+            end
+        end
+        if context.joker_main then
+            if pseudorandom('neopolitan') < G.GAME.probabilities.normal/3 then
+                return {
+                    message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}},
+                    mult_mod = card.ability.extra.mult, 
+                    colour = G.C.RED,
+                    card = self,
+                }
+            elseif pseudorandom('neopolitan') < G.GAME.probabilities.normal/2 then
+                ease_dollars(card.ability.extra.money)
+                G.GAME.dollar_buffer = (G.GAME.dollar_buffer or 0) + card.ability.extra.money
+                G.E_MANAGER:add_event(Event({func = (function() G.GAME.dollar_buffer = 0; return true end)}))
+                return {
+                    message = localize('$')..card.ability.extra.money,
+                    dollars = card.ability.extra.money,
+                    colour = G.C.MONEY
+                }
+            else
+                return {
+                    message = localize{type='variable',key='a_chips',vars={card.ability.extra.chips}},
+                    chip_mod = card.ability.extra.chips, 
+                    colour = G.C.CHIPS,
+                    card = self,
+                }
+            end
+        end
+    end
+}
+SMODS.Joker {
     key = 'strawberry',
     config = {
         extra = {mult = 20, mult_mod = 1},
@@ -1348,103 +1443,6 @@ SMODS.Joker {
         end
         if #G.consumeables.cards % 2 == 0 and #G.consumeables.cards ~= 0 then
             card.ability.extra.current_money = card.ability.extra.money * (#G.consumeables.cards/2)
-        end
-    end
-}
-
--- JokersV2
-SMODS.Joker {
-    key = 'neopolitan',
-    config = {
-        extra = {chips = 50, chip_mod = 10 , mult = 10, mult_mod = 2, money = 5, money_mod = 1},
-    },
-    atlas = 'Joker',
-    pos = { x = 14, y = 10 },
-    loc_txt = {
-        ['en-us'] = {
-            name = 'Neopolitan',
-            text = {
-                "{C:blue}+#3#{} Chips or {C:mult}+#1#{} Mult or {C:money}+$#5#{}",
-                "{C:blue}-#4#{} Chips, {C:mult}-#2#{} Mult, and {C:money}-$#6#{}",
-                "for every hand played",
-            }
-        }
-    },
-    rarity = 2,
-    cost = 6,
-    loc_vars = function(self, info_queue, card)
-        return { vars = { card.ability.extra.mult, card.ability.extra.mult_mod, card.ability.extra.chips, card.ability.extra.chip_mod, card.ability.extra.money, card.ability.extra.money_mod} }
-    end,
-    calculate = function(self, card, context)
-        if context.after and not context.blueprint then
-            if card.ability.extra.mult - card.ability.extra.mult_mod <= 0 or card.ability.extra.chips - card.ability.extra.chip_mod <= 0 or card.ability.extra.money - card.ability.extra.money_mod <= 0 then 
-                G.E_MANAGER:add_event(Event({
-                    func = function()
-                        play_sound('tarot1')
-                        card.T.r = -0.2
-                        card:juice_up(0.3, 0.4)
-                        card.states.drag.is = true
-                        card.children.center.pinch.x = true
-                        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
-                            func = function()
-                                    G.jokers:remove_card(card)
-                                    card:remove()
-                                    card = nil
-                                return true; end})) 
-                        return true
-                    end
-                })) 
-                return {
-                    message = localize('k_melted_ex'),
-                    colour = G.C.RED
-                }
-            else
-                card.ability.extra.mult = card.ability.extra.mult - card.ability.extra.mult_mod
-                card.ability.extra.chips = card.ability.extra.chips - card.ability.extra.chip_mod
-                card.ability.extra.money = card.ability.extra.money - card.ability.extra.money_mod
-                if 1 == 1 then
-                    return {
-                        message = localize{type='variable',key='a_mult_minus',vars={card.ability.extra.mult_mod}},
-                        colour = G.C.RED,
-                    }
-                end
-                if 1 == 1 then
-                    return {
-                        message = localize{type='variable',key='a_chips_minus',vars={card.ability.extra.chip_mod}},
-                        colour = G.C.CHIPS,
-                    }
-                end
-                return {
-                    message = localize('-$')..card.ability.extra.money_mod,
-                    colour = G.C.MONEY
-                }
-            end
-        end
-        if context.joker_main then
-            if pseudorandom('neopolitan') < G.GAME.probabilities.normal/3 then
-                return {
-                    message = localize{type='variable',key='a_mult',vars={card.ability.extra.mult}},
-                    mult_mod = card.ability.extra.mult, 
-                    colour = G.C.RED,
-                    card = self,
-                }
-            elseif pseudorandom('neopolitan') < G.GAME.probabilities.normal/2 then
-                ease_dollars(card.ability.extra.money)
-                G.GAME.dollar_buffer = (G.GAME.dollar_buffer or 0) + card.ability.extra.money
-                G.E_MANAGER:add_event(Event({func = (function() G.GAME.dollar_buffer = 0; return true end)}))
-                return {
-                    message = localize('$')..card.ability.extra.money,
-                    dollars = card.ability.extra.money,
-                    colour = G.C.MONEY
-                }
-            else
-                return {
-                    message = localize{type='variable',key='a_chips',vars={card.ability.extra.chips}},
-                    chip_mod = card.ability.extra.chips, 
-                    colour = G.C.CHIPS,
-                    card = self,
-                }
-            end
         end
     end
 }
@@ -2831,7 +2829,21 @@ SMODS.Booster{
 		return { vars = {card.draw_hand, card.config.center.config.choose, card.ability.extra} }
 	end,
 	create_card = function(self, card)
-		return create_card("Familiar_Planets", G.pack_cards, nil, nil, true, true, nil, 'fam_pantheon')
+        local _planet, _hand, _tally = nil, nil, 0
+        for k, v in ipairs(G.handlist) do
+            if G.GAME.hands[v].visible and G.GAME.hands[v].played > _tally then
+                _hand = v
+                _tally = G.GAME.hands[v].played
+            end
+        end
+        if _hand then
+            for k, v in pairs(G.P_CENTER_POOLS.Familiar_Planets) do
+                if v.config.hand_type == _hand then
+                    _planet = v.key
+                end
+            end
+        end
+		return create_card("Familiar_Planets", G.pack_cards, nil, nil, true, true, _planet, 'fam_pantheon')
 	end,
     update_pack = function(self, dt)
         if G.buttons then self.buttons:remove(); G.buttons = nil end
@@ -4235,15 +4247,6 @@ function copy_card(other, new_card, card_scale, playing_card, strip_edition)
 
     return new_card
 end
-
---local create_cardref = create_card
---function create_card(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
---    card = create_cardref(_type, area, legendary, _rarity, skip_materialize, soulable, forced_key, key_append)
---    if G.GAME.modifiers.fam_force_edition then 
---        card:set_edition(G.GAME.modifiers.fam_force_edition, true)
---    end
---    return card
---end
 
 fam_operators = {"+"}
 fam_numbers = {"0","1","2","3","4","5","6","7","8","9","10","25","m","nan","nil","???"}
