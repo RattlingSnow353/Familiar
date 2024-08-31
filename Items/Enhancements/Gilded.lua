2d9104e096bcad40d47b14d4d7814b638c8f1281
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2024-08-31 10:02:00 -0800

    Glided card fix
    card still does not count down

--- Items/Enhancements/Gilded.lua
+++ Items/Enhancements/Gilded.lua
@@ -7,14 +7,14 @@ local gilded = {
             "{C:money}$#1#{} when held in hand",
             "decreases by {C:money}$#2#{} each use",
             "becomes a {C:attention}steel card",
-            "after {C:attention}#1#{} uses.",
+            "after {C:attention}#3#{} uses.",
         }
     },
     pos = {x = 6, y = 0}, 
     atlas = 'Enhancers', 
-    config = { extra = {p_dollars = 5, dollar_mod = 1} },
+    config = { extra = {p_dollars = 5, dollar_mod = 1, left = 5} },
     loc_vars = function(self, info_queue, card)
-        return { vars = {self.config.extra.p_dollars, self.config.extra.dollar_mod} }
+        return { vars = {self.config.extra.p_dollars, self.config.extra.dollar_mod, self.config.extra.left } }
     end,
 }
 return {name = "Gilded", items = {gilded}}
