964d5860a9c5add5d6ac6a7302df323232e7a25e
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2024-08-30 20:25:58 -0800

    Layout fix +unusal & small content update

---
+++ Items/Vouchers/Sleight of Hand.lua
@@ -0,0 +1,32 @@
+local sleight_of_hand = {
+    object_type = "Voucher",
+    key = 'sleight_of_hand',
+    loc_txt = {
+        name = 'Sleight of Hand',
+        text = {
+            "{C:red}+#2#{} discard",
+            "{C:attention}+#1#{} hand size",
+        }
+    },
+    cost = 15,
+    atlas = 'Voucher',
+    pos = { x = 5, y = 1 },
+    config = { extra = {hand_size = 3, discards = 1}},
+    loc_vars = function(self, info_queue, card)
+        return { vars = {self.config.extra.hand_size, self.config.extra.discards} }
+    end,
+    requires={'v_fam_pickpocket'},
+    redeem = function(self, card, context)
+        G.E_MANAGER:add_event(Event({
+            func = function()
+                G.hand:change_size(card.ability.extra.hand_size)
+
+                G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.discards
+                ease_discard(card.ability.extra.discards)
+                return true
+            end
+        }))
+        G.GAME.pool_flags.sleight_of_hand_redeemed = true
+    end
+}
+return {name = "Sleight of Hand", items = {sleight_of_hand}}
