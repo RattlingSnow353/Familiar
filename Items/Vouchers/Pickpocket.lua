2b539476a0e61a27183011d8109495c79d210d59
Humplydinkle <verdehombre92@gmail.com>
2024-09-03 13:15:33 -0500

    balanced both vouchers (needs a second toss through to make sure visual stuff happens right away)

--- Items/Vouchers/Pickpocket.lua
+++ Items/Vouchers/Pickpocket.lua
@@ -4,24 +4,23 @@ local pickpocket = {
     loc_txt = {
         name = 'Pickpocket',
         text = {
-            "{C:blue}+#2#{} hand",
-            "{C:attention}+#1#{} hand size",
+            "{C:blue}+#1#{} hand size",
+            "{C:attention}-#2#{} discards",
         }
     },
     cost = 10,
     atlas = 'Voucher',
     pos = { x = 5, y = 0 },
-    config = { extra = {hand_size = 2, hands = 1}},
+    config = { extra = {hand_size = 2, discards = -1}},
     loc_vars = function(self, info_queue, card)
-        return { vars = {self.config.extra.hand_size, self.config.extra.hands} }
+        return { vars = {self.config.extra.hand_size, -self.config.extra.discards} }
     end,
     redeem = function(self, card, context)
         G.E_MANAGER:add_event(Event({
             func = function()
                 G.hand:change_size(card.ability.extra.hand_size)
-
-                G.GAME.starting_params.hands = G.GAME.starting_params.hands + card.ability.extra.hands
-                G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.hands
+                G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.discards
+                ease_discard(card.ability.extra.discards)
                 return true
             end
         }))
--- Items/Vouchers/Sleight of Hand.lua
+++ Items/Vouchers/Sleight of Hand.lua
@@ -4,23 +4,23 @@ local sleight_of_hand = {
     loc_txt = {
         name = 'Sleight of Hand',
         text = {
-            "{C:red}+#2#{} discard",
-            "{C:attention}+#1#{} hand size",
+            "{C:attention}+#1#{} hand",
+            "{C:attention}-#2#{} discard",
         }
     },
     cost = 15,
     atlas = 'Voucher',
     pos = { x = 5, y = 1 },
-    config = { extra = {hand_size = 3, discards = 1}},
+    config = { extra = {hands = 1, discards = -1}},
     loc_vars = function(self, info_queue, card)
-        return { vars = {self.config.extra.hand_size, self.config.extra.discards} }
+        return { vars = {self.config.extra.hands, -self.config.extra.discards} }
     end,
     requires={'v_fam_pickpocket'},
     redeem = function(self, card, context)
         G.E_MANAGER:add_event(Event({
             func = function()
-                G.hand:change_size(card.ability.extra.hand_size)
-
+                G.GAME.round_resets.hands = G.GAME.round_resets.hands + card.ability.extra.hands
+                G.GAME.starting_params.hands = G.GAME.starting_params.hands + card.ability.extra.hands
                 G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.discards
                 ease_discard(card.ability.extra.discards)
                 return true
