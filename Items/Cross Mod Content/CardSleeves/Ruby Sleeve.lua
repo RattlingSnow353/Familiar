25fc0068a9ecb91dfe3b052decae4f282d2f946c
Humplydinkle <verdehombre92@gmail.com>
2024-09-04 21:43:35 -0500

    CardSleeve balance update

--- Items/Cross Mod Content/CardSleeves/Ruby Sleeve.lua
+++ Items/Cross Mod Content/CardSleeves/Ruby Sleeve.lua
@@ -8,13 +8,19 @@ if (SMODS.Mods["CardSleeves"] or {}).can_load then
         config = {
         },
         loc_vars = function(self)
-        self.config = {consumables = { 'c_fam_playback', 'c_fam_playback' }, discards = 2}
+        self.config = {
+            consumables = { 'c_fam_playback', 'c_fam_playback' }, 
+            discards = 2,
+            hands = -1
+        }
         end,
         loc_txt = {
             name = "Ruby Sleeve",
             text = {
-                "Start with {C:attention}2 copies{} of Playback,",
+                "Start with {C:attention}2 copies{}",
+                "of Playback,",
                 "{C:red}+2{} discards every round.",
+                "{C:blue}-1{} hand every round."
             }
         }
     }
