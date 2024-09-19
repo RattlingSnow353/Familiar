cfa1b5e606025ef43f996e58c5e9e79315468073
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2024-09-18 17:02:30 -0800

    Retrigger Api fix

--- Items/Jokers/Thinktank.lua
+++ Items/Jokers/Thinktank.lua
@@ -22,22 +22,31 @@ local thinktank = {
         return { vars = {  } }
     end,
     calculate = function(self, card, context)
-        local jokers = G.jokers.cards
-        other_joker = jokers[#jokers] 
-        if not other_joker then
-            return 
-        end
-        context.blueprint = (context.blueprint and (context.blueprint + 1)) or 1
-        context.blueprint_card = context.blueprint_card or card
-        if context.blueprint > #G.jokers.cards + 1 then 
-            return 
-        end
-        local other_joker_ret = other_joker:calculate_joker(context)
-        if other_joker_ret then 
-            other_joker_ret.card = context.blueprint_card or card
-            other_joker_ret.colour = G.C.RED
-            return other_joker_ret
-        end
+        local other_joker = nil
+		for i = 1, #G.jokers.cards do
+			if G.jokers.cards[i] == card then
+				other_joker = G.jokers.cards[#G.jokers.cards]
+			end
+		end
+		if other_joker and other_joker ~= self then
+			context.blueprint = (context.blueprint and (context.blueprint + 1)) or 1
+			context.blueprint_card = context.blueprint_card or card
+
+			if context.blueprint > #G.jokers.cards + 1 then
+				return
+			end
+
+			local other_joker_ret, trig = other_joker:calculate_joker(context)
+			if other_joker_ret or trig then
+				if not other_joker_ret then
+					other_joker_ret = {}
+				end
+				other_joker_ret.card = context.blueprint_card or card
+				other_joker_ret.colour = G.C.BLUE
+				other_joker_ret.no_callback = true
+				return other_joker_ret
+			end
+		end
     end
 }
 return {name = "Jokers", items = {thinktank}}
