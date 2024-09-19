e0e527e52b26de847fa90b88b5f10939c30ac550
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2024-09-18 17:22:40 -0800

    Speckled Text fix

--- Items/Editions/Speckled.lua
+++ Items/Editions/Speckled.lua
@@ -16,9 +16,9 @@ local speckle = {
             "{C:red}+rand(){} Mult"
         }
     },
-    config = { ranmult = 0, ranchips = 0, mmin = 1, mmax = 5, cmin = 1, cmax = 25, rantextnum =  fam_numbers[math.random(1, 15)], rantextnum2 =  fam_numbers[math.random(1, 15)] },
+    config = { mmin = 1, mmax = 5, cmin = 1, cmax = 25 },
     loc_vars = function(self, info_queue)
-        return {vars = {self.config.ranmult, self.config.ranchips, self.config.rantextnum, self.config.rantextnum2}}
+        return {vars = {}}
     end,
 
     in_shop = true,
@@ -27,6 +27,142 @@ local speckle = {
     get_weight = function(self)
         return G.GAME.edition_rate * self.weight
     end,
+    calculate = function(self, card, context)
+		if context.edition_main and context.edition_val then
+			context.edition_val.mult_mod = pseudorandom("cry_noisy_mult", self.config.mmin, self.config.mmax)
+			context.edition_val.chip_mod = pseudorandom("cry_noisy_chips", self.config.cmin, self.config.cmax)
+		end
+	end,
+	generate_ui = function(self, info_queue, card, desc_nodes, specific_vars, full_UI_table)
+		if not full_UI_table.name then
+			full_UI_table.name = localize({ type = "name", set = self.set, key = self.key, nodes = full_UI_table.name })
+		end
+		local r_mults = {}
+		for i = self.config.mmin, self.config.mmax do
+			r_mults[#r_mults + 1] = tostring(i)
+		end
+		local loc_mult = " " .. (localize("k_mult")) .. " "
+		local r_chips = {}
+		for i = self.config.cmin, self.config.cmax do
+			r_chips[#r_chips + 1] = tostring(i)
+		end
+		local loc_chips = " Chips "
+		mult_ui = {
+			{ n = G.UIT.T, config = { text = "  +", colour = G.C.MULT, scale = 0.32 } },
+			{
+				n = G.UIT.O,
+				config = {
+					object = DynaText({
+						string = r_mults,
+						colours = { G.C.MULT },
+						pop_in_rate = 9999999,
+						silent = true,
+						random_element = true,
+						pop_delay = 0.5,
+						scale = 0.32,
+						min_cycle_time = 0,
+					}),
+				},
+			},
+			{
+				n = G.UIT.O,
+				config = {
+					object = DynaText({
+						string = {
+							{ string = "rand()", colour = G.C.JOKER_GREY },
+							{
+								string = "#@"
+									.. (G.deck and G.deck.cards[1] and G.deck.cards[#G.deck.cards].base.id or 11)
+									.. (
+										G.deck
+											and G.deck.cards[1]
+											and G.deck.cards[#G.deck.cards].base.suit:sub(1, 1)
+										or "D"
+									),
+								colour = G.C.RED,
+							},
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+							loc_mult,
+						},
+						colours = { G.C.UI.TEXT_DARK },
+						pop_in_rate = 9999999,
+						silent = true,
+						random_element = true,
+						pop_delay = 0.2011,
+						scale = 0.32,
+						min_cycle_time = 0,
+					}),
+				},
+			},
+		}
+		chip_ui = {
+			{ n = G.UIT.T, config = { text = "  +", colour = G.C.CHIPS, scale = 0.32 } },
+			{
+				n = G.UIT.O,
+				config = {
+					object = DynaText({
+						string = r_chips,
+						colours = { G.C.CHIPS },
+						pop_in_rate = 9999999,
+						silent = true,
+						random_element = true,
+						pop_delay = 0.5,
+						scale = 0.32,
+						min_cycle_time = 0,
+					}),
+				},
+			},
+			{
+				n = G.UIT.O,
+				config = {
+					object = DynaText({
+						string = {
+							{ string = "rand()", colour = G.C.JOKER_GREY },
+							{
+								string = "@#"
+									.. (G.deck and G.deck.cards[1] and G.deck.cards[1].base.suit:sub(2, 2) or "m")
+									.. (G.deck and G.deck.cards[1] and G.deck.cards[1].base.id or 7),
+								colour = G.C.BLUE,
+							},
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+							loc_chips,
+						},
+						colours = { G.C.UI.TEXT_DARK },
+						pop_in_rate = 9999999,
+						silent = true,
+						random_element = true,
+						pop_delay = 0.2011,
+						scale = 0.32,
+						min_cycle_time = 0,
+					}),
+				},
+			},
+		}
+		desc_nodes[#desc_nodes + 1] = mult_ui
+		desc_nodes[#desc_nodes + 1] = chip_ui
+	end,
 
     shader = 'speckle'
 }
