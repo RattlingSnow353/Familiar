ce4b8d76fc0feef92362d77f19ea05a3f763b005
Humplydinkle <verdehombre92@gmail.com>
2025-01-15 10:04:24 -0600

    Added Professor and Bard (no art yet)

---
+++ Items/Jokers/Bard.lua
@@ -0,0 +1,37 @@
+local bard =  {
+    object_type = "Joker",
+    key = "bard",
+    config = {
+        extra =  {hand_size = -2, discard_size = 1},
+    },
+    atlas = 'Joker',
+    pos = { x = 0, y = 0 },
+    loc_txt = {
+        ['en-us'] = {
+            name = 'Bard',
+            text = {
+                "{C:attention}#1#{} hand size,",
+                "{C:mult}+#2#{} discards",
+                ""
+            }
+        }
+    },
+    rarity = 1,
+    cost = 7,
+    blueprint_compat = false,
+    loc_vars = function(self, info_queue, card)
+        return { vars = { card.ability.extra.hand_size, card.ability.extra.discard_size} }
+    end,
+    add_to_deck = function(self, card, from_debuff)    
+        G.hand:change_size(card.ability.extra.hand_size)
+        G.GAME.round_resets.discards = G.GAME.round_resets.discards + card.ability.extra.discard_size
+        ease_discard(card.ability.extra.discard_size)
+
+    end,
+    remove_from_deck = function(self, card, from_debuff)
+        G.GAME.round_resets.discards = G.GAME.round_resets.discards - card.ability.extra.discard_size
+        G.hand:change_size(-card.ability.extra.hand_size)
+        ease_discard(-card.ability.extra.discard_size)
+    end
+}
+return {name = "Jokers", items = {bard}}
