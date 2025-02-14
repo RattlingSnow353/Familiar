2d34cf8025559ae5e55d55a3627f0dda3b9e8001
RattlingSnow353 <84494581+RattlingSnow353@users.noreply.github.com>
2025-02-14 16:48:51 -0500

    Wrong Branch [Fixed Edtions]
    whoops

--- Items/Editions/Static.lua
+++ Items/Editions/Static.lua
@@ -15,9 +15,9 @@ local statics = {
             "{X:chips,C:white} X#1# {} Chips"
         }
     },
-    config = { x_chips = 1.5 },
+    config = { Xchips = 1.5 },
     loc_vars = function(self, info_queue)
-        return {vars = {self.config.x_chips}}
+        return {vars = {self.config.Xchips}}
     end,
 
     in_shop = true,
@@ -25,6 +25,19 @@ local statics = {
     extra_cost = 5,
     get_weight = function(self)
         return G.GAME.edition_rate * self.weight
+    end,
+    calculate = function(self, card, context)
+        if context.post_joker or (context.main_scoring and context.cardarea == G.play) then
+            return {func = function()
+                local xchips = G.P_CENTERS.e_fam_statics.config.Xchips
+                hand_chips = mod_chips(hand_chips * xchips)
+                update_hand_text({delay = 0}, {chips = hand_chips})
+                card_eval_status_text(card, 'extra', nil, percent, nil,
+                {message = 'X'..xchips..' Chips',
+                edition = true,
+                x_chips = true})
+            end}
+        end
     end,
 
     shader = 'statics'
